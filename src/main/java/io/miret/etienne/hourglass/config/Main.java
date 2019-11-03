3c20ac3d7d9bf8da7da921708f84c1f410b04bd3
Etienne Miret <etienne.miret@ens-lyon.org>
2019-11-03 18:06:08 +0100

    Package as a WAR.

--- src/main/java/io/miret/etienne/hourglass/config/Main.java
+++ src/main/java/io/miret/etienne/hourglass/config/Main.java
@@ -2,6 +2,8 @@ package io.miret.etienne.hourglass.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +14,15 @@ import java.time.ZoneId;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan ("io.miret.etienne.hourglass")
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
   public static void main (String[] args) {
     SpringApplication.run (Main.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+    return  builder.sources (Main.class);
   }
 
   @Bean
