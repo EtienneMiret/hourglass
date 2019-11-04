551042301aecb7753948d0faa1186e6b4819a2f1
Etienne Miret <etienne.miret@ens-lyon.org>
2019-11-04 22:00:13 +0100

    Display only one page at a time.

--- src/main/ts/components/App.tsx
+++ src/main/ts/components/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import * as React from 'react';
 import { Hello } from './Hello';
@@ -6,13 +6,15 @@ import { GlobalState } from '../state';
 import { AnyAction, Store } from 'redux';
 import { UserListContainer } from '../containers/user-list';
 
-const MyHello = () => <Hello compiler="TypeScript" framework="Redux/React"/>;
-
 export const App = ({store}: {store: Store<GlobalState, AnyAction>}) => (
     <Provider store={store}>
       <Router>
-        <Route path="/" component={MyHello} />
-        <Route path="/users" component={UserListContainer}/>
+        <Switch>
+          <Route path="/users" component={UserListContainer}/>
+          <Route path="/">
+            <Hello compiler="TypeScript" framework="Redux/React"/>
+          </Route>
+        </Switch>
       </Router>
     </Provider>
 );
