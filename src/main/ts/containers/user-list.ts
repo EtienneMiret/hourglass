1faf8de44713926500ed57dba127fada23340424
Etienne Miret <etienne.miret@ens-lyon.org>
2019-11-06 23:25:33 +0100

    Sort user list by name.

--- src/main/ts/containers/user-list.ts
+++ src/main/ts/containers/user-list.ts
@@ -3,8 +3,10 @@ import { UserList, UserListProps } from '../components/UserList';
 import { GlobalState } from '../state';
 
 function mapStateToProps (state: GlobalState): UserListProps {
+  const users = Object.values (state.users.list);
+  users.sort ((a, b) => a.name.localeCompare (b.name));
   return {
-    users: Object.values (state.users.list),
+    users,
     status: state.users.status
   };
 }
