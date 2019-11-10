aa683bef7b3b9a0b3234f5c83201504f94858ce3
Etienne Miret <etienne.miret@ens-lyon.org>
2019-11-10 13:23:18 +0100

    Provide roles to authenticated users.

--- src/main/java/io/miret/etienne/hourglass/data/config/SecurityConfiguration.java
+++ src/main/java/io/miret/etienne/hourglass/data/config/SecurityConfiguration.java
@@ -3,13 +3,13 @@ package io.miret.etienne.hourglass.data.config;
 import lombok.Getter;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Getter
 public class SecurityConfiguration {
 
-  private List<String> prefects;
+  private Set<String> prefects;
 
   private Map<String, OAuth2ClientProperties.Registration> registrations;
 
