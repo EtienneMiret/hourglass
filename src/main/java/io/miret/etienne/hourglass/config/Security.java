6005caf9bd77320a91e72c98e31cf3e6a005cc09
Etienne Miret <etienne.miret@ens-lyon.org>
2019-11-10 13:48:35 +0100

    Authorize access only for registered users.

--- src/main/java/io/miret/etienne/hourglass/config/Security.java
+++ src/main/java/io/miret/etienne/hourglass/config/Security.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
@@ -15,9 +17,17 @@ import static org.springframework.boot.autoconfigure.security.oauth2.client.OAut
 
 @Configuration
 @AllArgsConstructor
-public class Security {
+public class Security extends WebSecurityConfigurerAdapter {
 
   private final AppConfiguration configuration;
+
+  @Override
+  protected void configure (HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests ().anyRequest ().hasRole ("student").and ()
+        .csrf ().disable ()
+        .oauth2Login ();
+  }
 
   @Bean
   public ClientRegistrationRepository clientRegistrationRepository () {
