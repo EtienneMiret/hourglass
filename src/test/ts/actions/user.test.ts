e0244c1afc1fac4605a5128abf908aa5c69a666a
Etienne Miret <etienne.miret@ens-lyon.org>
2019-11-11 07:54:10 +0100

    Test user actions.

---
+++ src/test/ts/actions/user.test.ts
@@ -0,0 +1,37 @@
+import {
+  FETCH_SINGLE_USER_FAILURE,
+  FETCH_SINGLE_USER_REQUEST,
+  FETCH_SINGLE_USER_SUCCESS, fetchSingleUserFailure,
+  fetchSingleUserRequest,
+  fetchSingleUserSuccess
+} from '../../../main/ts/actions/user';
+
+describe ('User actions', () => {
+
+  test ('fetchSingleUserRequest ()', () => {
+    const action = fetchSingleUserRequest ('foo');
+
+    expect (action.type).toBe (FETCH_SINGLE_USER_REQUEST);
+    expect (action.id).toBe ('foo');
+  });
+
+  test ('fetchSingleUserSuccess ()', () => {
+    const user = {
+      id: 'foo',
+      name: 'Foo',
+      emails: ['foo@example.com']
+    };
+    const action = fetchSingleUserSuccess (user);
+
+    expect (action.type).toBe (FETCH_SINGLE_USER_SUCCESS);
+    expect (action.response).toBe (user);
+  });
+
+  test ('fetchSingleUserFailure ()', () => {
+    const action = fetchSingleUserFailure ('foo');
+
+    expect (action.type).toBe (FETCH_SINGLE_USER_FAILURE);
+    expect (action.id).toBe ('foo');
+  });
+
+});
