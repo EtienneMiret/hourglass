a4f5e070fd5ff5be20c248fa782932c3fa9587ce
Etienne Miret <etienne.miret@ens-lyon.org>
2020-02-15 19:41:52 +0100

    Allow prefects to create users.

--- src/main/ts/components/UserList.tsx
+++ src/main/ts/components/UserList.tsx
@@ -1,31 +1,69 @@
 import * as React from 'react';
-import { User } from '../state/user';
+import { NewUser, User } from '../state/user';
 import { HttpStatus } from '../state/status';
 import { UserListItem } from './UserListItem';
 import { Loader } from './Loader';
 import { useTranslation } from 'react-i18next';
+import { UserEditContainer } from '../containers/user-edit';
 
-export interface UserListProps {
-  fetchUsers: () => {},
-  users: User[],
-  status: HttpStatus
+export interface UserListStateProps {
+  prefect: boolean;
+  creation: NewUser | null;
+  users: User[];
+  status: HttpStatus;
 }
+
+export interface UserListDispatchProps {
+  fetchUsers: () => {};
+  startCreate: () => {};
+}
+
+export type UserListProps = UserListStateProps & UserListDispatchProps;
 
 export const UserList = (props: UserListProps) => {
   const {t} = useTranslation ();
 
-  switch (props.status) {
-    case HttpStatus.None:
-      props.fetchUsers ();
-      return <div/>;
-    case HttpStatus.Progressing:
-      return <Loader/>;
-    case HttpStatus.Success:
-      if (props.users.length === 0) {
-        return <div>{t('users.none')}</div>
-      }
-      return <ol>{props.users.map (user => <UserListItem user={user} key={user.id}/>)}</ol>;
-    case HttpStatus.Failure:
-      return <div>{t('users.loading-failed')}</div>
+  function list () {
+    switch (props.status) {
+      case HttpStatus.None:
+        props.fetchUsers ();
+        return <div/>;
+      case HttpStatus.Progressing:
+        return <Loader/>;
+      case HttpStatus.Success:
+        if (props.users.length === 0) {
+          return <div>{t('users.none')}</div>
+        }
+        return <ol>{props.users.map (user => <UserListItem user={user} key={user.id}/>)}</ol>;
+      case HttpStatus.Failure:
+        return <div>{t('users.loading-failed')}</div>
+    }
   }
+
+  function actions () {
+    const actions: JSX.Element[] = [];
+    actions.push (<button onClick={props.fetchUsers} key="reload">{t ('actions.reload')}</button>);
+
+    if (props.prefect && props.status === HttpStatus.Success) {
+      actions.push (<button onClick={props.startCreate} key="create">{t ('actions.add')}</button>);
+    }
+
+    return <div className="actions">
+      {actions}
+    </div>;
+  }
+
+  function popup () {
+    if (props.prefect && props.creation) {
+      return <UserEditContainer user={props.creation}/>;
+    }
+
+    return <div/>;
+  }
+
+  return <div className="user-list">
+    {list ()}
+    {actions ()}
+    {popup ()}
+  </div>;
 };
--- src/main/ts/containers/user-list.ts
+++ src/main/ts/containers/user-list.ts
@@ -1,23 +1,43 @@
 import { connect } from 'react-redux';
 import { GlobalState } from '../state';
 import { fetchUsers } from '../actions/users';
-import { UserListBlock } from '../components/UserListBlock';
+import {
+  UserList,
+  UserListDispatchProps,
+  UserListStateProps
+} from '../components/UserList';
+import { ThunkDispatch } from 'redux-thunk';
+import { Action } from 'redux';
+import { editUserStart } from '../actions/user-edition';
+import { HttpStatus } from '../state/status';
 
-function mapStateToProps (state: GlobalState) {
+function mapStateToProps (state: GlobalState): UserListStateProps {
   const users = Object.values (state.users.list)
       .map (u => u.user);
   users.sort ((a, b) => a.name.localeCompare (b.name));
   return {
+    prefect: state.whoami.status === HttpStatus.Success && state.whoami.whoami!.prefect,
+    creation: state.users.creation === null ? null : state.users.creation.user,
     users,
     status: state.users.status
   };
 }
 
-const mapDispatchToProps = {
-  fetchUsers
-};
+function mapDispatchToProps (
+    dispatch: ThunkDispatch<GlobalState, undefined, Action>
+): UserListDispatchProps {
+  return {
+    fetchUsers: () => dispatch (fetchUsers ()),
+    startCreate: () => dispatch (editUserStart (null))
+  }
+}
 
-export const UserListContainer = connect(
+export const UserListContainer = connect<
+    UserListStateProps,
+    UserListDispatchProps,
+    {},
+    GlobalState
+> (
     mapStateToProps,
     mapDispatchToProps
-) (UserListBlock);
+) (UserList);
